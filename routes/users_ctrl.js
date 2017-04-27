635ee47d4a2f2552efd94e68b54e6e8c7acba507
Nicolas RIFFARD <nicolas.riffard@grenoble-inp.org>
2017-04-27 17:56:14 +0200

    routes

--- routes/users_ctrl.js
+++ routes/users_ctrl.js
@@ -2,17 +2,11 @@ var express = require('express');
 var router = express.Router();
 var service = require('../services/users');
 
-
-/*router.put('/user/', service.updateUserInfo);
-router.post('/user/', service.addUser);
-router.get('/user/', service.getUserInfo);
-router.delete('/user/', service.deleteUserInfo);
-
-router.get('/user/contacts/:idUser', service.getContactList);
-router.get('/user/location/:idUser', service.getUserLocation);
-router.put('/user/location/:idUser', service.updateUserLocation);
-
-*/
+router.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 router.get('/users/', service.listAllPhones);
 router.get('/user/:phone', service.isThePhone);
