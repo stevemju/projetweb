d42bb9d520758d195a32fdcd0cbc861605ed6fca
Nicolas RIFFARD <nicolas.riffard@grenoble-inp.org>
2017-04-27 15:07:21 +0200

    sockets ON

--- db/pg_users.js
+++ db/pg_users.js
@@ -1,37 +1,6 @@
 var pgp = require('pg-promise')(/*options*/)
 var dbconfig = require('../config/settings.js').settings
 var db = pgp(dbconfig)
-/*
-
-function updateUserInfo(callback)
-{
-}
-
-function addUser(callback)
-{
-}
-
-function getUserInfo(callback)
-{
-}
-
-function deleteUserInfo(callback)
-{
-}
-
-function getContactList(callback)
-{
-}
-
-function getUserLocation(callback)
-{
-}
-
-function updateUserLocation(callback)
-{
-}
-
-*/
 
 function getAllPhones(callback) {
 	var requete = 'select phone from public.users'
--- main.js
+++ main.js
@@ -8,6 +8,22 @@ var users_services = require('./services/users')
 
 app.use('/api/', users_router);
 
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+
+	socket.send('Un client vient de se connecter');
+    
+    
+    // on écoute la future déconnection de la socket nouvellement créée
+    //socket_client.on('disconnect', function(){
+     //   nbclient--; 
+        // on prévient tout le monde du nouveau nombre de 
+        // clients sur l'événement broadcast
+      //  io.sockets.emit('broadcast', nbclient) 
+    //})
+})
+
 /*io.on('connect', function (socket) {
     console.log("Start animation");
     surface_services.animationOn(socket)
